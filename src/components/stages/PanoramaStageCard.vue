15f36060e7ee3ba76f348a02e509c82858c1c585
Terry Jia <terryjia88@gmail.com>
2026-06-11 11:15:25 -0400

    fix(stage-cards) #6: forward node prop to <StageCard> (#9)

--- src/components/stages/PanoramaStageCard.vue
+++ src/components/stages/PanoramaStageCard.vue
@@ -9,6 +9,7 @@
 
     <StageCard
       :state="state"
+      :node="node"
       :on-run-request="onRunRequest"
       :on-cancel-request="onCancelRequest"
       :on-disconnect="onDisconnect"
--- src/components/stages/RotateStageCard.vue
+++ src/components/stages/RotateStageCard.vue
@@ -43,6 +43,7 @@
 
     <StageCard
       :state="state"
+      :node="node"
       :on-run-request="onRunRequest"
       :on-cancel-request="onCancelRequest"
       :on-disconnect="onDisconnect"
