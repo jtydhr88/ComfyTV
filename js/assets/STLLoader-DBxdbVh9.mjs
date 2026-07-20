e3297d0f19ad001165826ecccceb4e11fdc41336
Terry Jia <terryjia88@gmail.com>
2026-07-19 22:54:20 -0400

    feat(proxy): auto-build proxies for large stage output videos

--- js/assets/STLLoader-DBxdbVh9.mjs
+++ js/assets/STLLoader-DBxdbVh9.mjs
@@ -1,4 +1,4 @@
-import { w as Loader, x as FileLoader, B as BufferGeometry, C as Color, i as SRGBColorSpace, H as BufferAttribute, V as Vector3, F as Float32BufferAttribute } from "./main-0x5DWLWJ.mjs";
+import { w as Loader, x as FileLoader, B as BufferGeometry, C as Color, i as SRGBColorSpace, H as BufferAttribute, V as Vector3, F as Float32BufferAttribute } from "./main-BV14oSHQ.mjs";
 class STLLoader extends Loader {
   /**
    * Constructs a new STL loader.
@@ -208,4 +208,4 @@ class STLLoader extends Loader {
 export {
   STLLoader
 };
-//# sourceMappingURL=STLLoader-DYkWKqrV.mjs.map
+//# sourceMappingURL=STLLoader-DBxdbVh9.mjs.map
